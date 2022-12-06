08ace810bc056b077a5f921f6810335847023150
Sixtine Delvallée <sixtine.delvallee@gmail.com>
2022-12-06 09:44:02 +0100

    fix installation tailwind

--- back-end-project/resources/views/index.blade.php
+++ back-end-project/resources/views/index.blade.php
@@ -1,19 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<h1>Test</h1>
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body>
+    <h1>Test</h1>
 
-@dump($movies)
-@foreach($movies as $movie)
-    <div class="movies_container">
-        <h1>{{$movie['title']}}</h1>
-        <h2>{{$movie['original_title']}}</h2>
-        <img src="https://image.tmdb.org/t/p/w500{{$movie['poster_path']}}"/>
-        <p>{{$movie['overview']}}</p>
-        <p>{{$movie['release_date']}}</p>
-    </div>
-@endforeach
-<?php
-print_r($movies)
-?>
+    @dump($movies)
+    @foreach($movies as $movie)
+        <div class="movies_container">
+            <h1>{{$movie['title']}}</h1>
+            <h2 class="font-bold text-amber-600">{{$movie['original_title']}}</h2>
+            <img src="https://image.tmdb.org/t/p/w500{{$movie['poster_path']}}"/>
+            <p>{{$movie['overview']}}</p>
+            <p>{{$movie['release_date']}}</p>
+        </div>
+    @endforeach
+    <?php
+    print_r($movies)
+    ?>
+</body>
+</html>
 
 <style>
     .movies_container{
