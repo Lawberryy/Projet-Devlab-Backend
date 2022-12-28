becc3704e2e7d0754811770e05dddca4fd2ca688
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2022-12-28 22:00:39 +0100

    laravel breeze connexion setup & test :sparkles:

--- back-end-project/tailwind.config.js
+++ back-end-project/tailwind.config.js
@@ -1,11 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./resources/**/*.blade.php",
-      "./resources/**/*.js",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './index.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}',
+        "./resources/**/*.js",
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
