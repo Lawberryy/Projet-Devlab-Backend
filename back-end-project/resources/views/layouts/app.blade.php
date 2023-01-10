9a2eb5a630111a36e04d484400680e6cd153aaa6
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2023-01-10 09:40:27 +0100

    last push

--- back-end-project/resources/views/layouts/app.blade.php
+++ back-end-project/resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-mid-blue-main">
+        <div class="min-h-screen bg-gray-300">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
